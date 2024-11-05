8937d2d9f7ec7fe1c8a7f5dbc6abcd80c5b16ff2
anbua <anbua@Anbuarun>
2024-11-05 14:24:48 +0530

    day3

---
+++ C2TC_ARUNKUMAR/src/day3encap/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package day3encap;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		//object creation 
+		OopsConceptDemo obj = new
+		OopsConceptDemo();
+		obj.setSerialNum(13);
+		obj.setName("Arun");
+		obj.setAge(21);
+		System.out.println(obj);
+		}
+
+}
---
+++ C2TC_ARUNKUMAR/src/day3inheri/SLInheritanceDemo.java
@@ -0,0 +1,9 @@
+package day3inheri;
+
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+		Student student = new Student("arun", "7339152831", "cuddalore", 9888878787L, 10, "City College");
+		System.out.println(student);
+		}
+}
