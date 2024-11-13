b70c401c76c17c0a1f91cdfdb771950958e3ebea
anbua <anbua@Anbuarun>
2024-11-13 07:10:37 +0530

    day13,14

---
+++ C2TC_ARUNKUMAR/src/dayfourteen/synchronization/InsufficientBalanceException.java
@@ -0,0 +1,12 @@
+//Program to demonstrate User Defined Exception class
+package dayfourteen.synchronization;
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException() {
+		super("Insufficient balance in your account");
+	}
+
+	public InsufficientBalanceException(String message) {
+		super(message);
+	}
+}
